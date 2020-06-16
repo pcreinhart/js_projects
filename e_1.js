b1deea2719d9d74a5b3bd043d50120d766ff8dd5
paulreinhart <reinhart.paul@hotmail.com>
2020-06-15 17:34:47 -0700

    One more time

---
+++ e_1.js
@@ -0,0 +1,16 @@
+console.log('You are writing to the screen')
+console.log('This is called standard input')
+console.log("This stuff in the () is a string")
+console.log("Start a string with double-quote")
+console.log("Write whatever you wannnaa ")
+console.log("Then end with a double quote")
+console.log("You cannot break lines like you can in HTML")
+//This is a comment, everthing after is ignore. 
+console.log("Will this get ignore ?  I it after it")
+/* This is also a comment. You use this for longer things that you want to say 
+*even longer ....
+*/
+console.log("So I am")
+// The code inside will not run
+/* console.log("I will not run")
+*/
