7952e19b344c672bb88574ea887dcf8e80123c5e
paulreinhart <reinhart.paul@hotmail.com>
2020-09-16 09:17:55 -0700

    Create es_5.js

---
+++ es_5.js
@@ -0,0 +1,13 @@
+const invincible = 94;
+let changeme = 82;
+
+console.log(`invincible=${invincible} but changeme =${changeme}`);
+
+
+
+
+let goose = 90
+console.log (goose)
+console.log (changeme)
+changeme = 1000;
+console.log (changeme)
