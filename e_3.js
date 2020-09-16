a3b518d5059c6449da5eefeec849b018fed132ed
paulreinhart <reinhart.paul@hotmail.com>
2020-09-16 09:19:38 -0700

    Create e_3.js

---
+++ e_3.js
@@ -0,0 +1,8 @@
+// You can give multiple things for console.log to pring.
+// You need to separate each thing with a comma.
+console.log("Name:",'PaulReinhart');
+console.log("Age:", 43);
+console.log("Feet Tall:", Math.round (74/12));
+console.log("And inches:",74-(Math.round(74/12)*12));
+console.log("Age*Height:", 43*74);
+console.log("Age*Feet:", 43* Math.round(74/12));
