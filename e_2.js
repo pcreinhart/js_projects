6b31dbde1d3b8a952a532eee16eb3181e753b88c
paulreinhart <reinhart.paul@hotmail.com>
2020-09-16 09:19:49 -0700

    Create e_2.js

---
+++ e_2.js
@@ -0,0 +1,18 @@
+//This is a comment, everything after is ignored.
+
+console.log ("I am Code");
+
+// This is a comment that is left open so everthing that follows is normal
+
+console.log ("I am also groot");
+
+/*This is also a comment.  Use this for longer stuff like encyclopedia of stuff.
+
+/ * This is the same comment but I do this 
+* to keep it in view of the editor instead
+* of having to scrool etc. 
+*/
+console.log ("So I am");
+// The code inside the next comment will not run:
+/* console.log ("You will never see me")'
+*/
